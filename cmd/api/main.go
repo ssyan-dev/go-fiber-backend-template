b2272cecaa12f02586c28641807fba09c0187ac2
Stanislav Simakhin <me@ssyan.ru>
2026-06-01 22:44:51 +0300

    feat: add 'uber/zap' logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"time"
 
 	"github.com/ssyan-dev/go-fiber-backend-template/internal/config"
 	"github.com/ssyan-dev/go-fiber-backend-template/internal/database"
+	"github.com/ssyan-dev/go-fiber-backend-template/internal/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	
+
 	cfg, err := config.InitConfig()
 	if err != nil {
-		log.Fatalln(err.Error())
+		panic(err.Error())
 	}
+
+	l := logger.NewLogger(cfg.App.Env)
+	defer l.Sync()
+
+	l.Info("starting backend",
+		zap.String("env", cfg.App.Env),
+		zap.Int("port", cfg.App.Port),
+	)
 
 	pg, err := database.NewPostgres(ctx, &cfg.Postgres)
 	if err != nil {
-		log.Fatalln(err.Error())
+		l.Fatal(err.Error())
 	}
 	defer pg.Close()
+	l.Info("postgres connected!")
 
 	rdb, err := database.NewRedis(ctx, &cfg.Redis)
 	if err != nil {
-		log.Fatalln(err.Error())
+		l.Fatal(err.Error())
 	}
 	defer rdb.Close()
-
-	fmt.Println(cfg)
+	l.Info("redis connected!")
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.28.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 )
